2cd71f30672aa06f47ec92663932361b478cad27
golovpavel <golovpasha@gmail.com>
2020-02-18 00:54:10 +0300

    Created main window interface

--- src/model/quantity.py
+++ src/model/quantity.py
@@ -33,10 +33,11 @@ class PhysicalQuantity:
 
 class Unit:
 
-    def __init__(self, name, is_base_unit, conversion_factor):
+    def __init__(self, name, is_base_unit, conversion_factor, conversion_operation):
         self.name = name
         self.is_base_unit = is_base_unit
         self.conversion_factor = conversion_factor
+        self.conversion_operation = conversion_operation
 
     def __str__(self):
         return self.name
@@ -46,4 +47,5 @@ class Unit:
 
     @staticmethod
     def decode_from_json(json_obj):
-        return Unit(json_obj["name"], bool(json_obj["is_base_unit"]), json_obj["conversion_factor"])
+        return Unit(json_obj["name"], bool(json_obj["is_base_unit"]), json_obj["conversion_factor"],
+                    json_obj["conversion_operation"])

3c6ef0781734a10b45747d948b4553c81b1f48f0
golovpeter <petya-golov@mail.ru>
2020-03-22 00:49:22 +0300

    Changed layout of converter frame

--- src/frames/converter_frame.py
+++ src/frames/converter_frame.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showerror
+
 from constants.frames import MAIN_FRAME_NAME
 
 
@@ -19,31 +20,42 @@ class ConverterFrame(tk.Frame):
         self.grid_rowconfigure(4, weight=1)
 
         self.main_label = tk.Label(self, font="Helvetica 30 bold")
-        self.main_label.grid(row=0, columnspan=3, pady=40)
+        self.main_label.pack(pady=35)
+
+        self.first_info_label = tk.Label(self, text="Изначальная единица измерения", font="Helvetica 15")
+        self.first_info_label.pack(pady=5)
 
         self.first_unit_selector = ttk.Combobox(self, values=list(self.units.keys()), justify='center',
                                                 textvariable=self.selected_unit_one)
         self.first_unit_selector['state'] = 'readonly'
-        self.first_unit_selector.grid(row=1, column=0)
+        self.first_unit_selector.pack()
+
+        self.second_info_label = tk.Label(self, text="Конвертируемое значение", font="Helvetica 15")
+        self.second_info_label.pack(pady=5)
+
+        self.first_entry = tk.Entry(self, width=24)
+        self.first_entry.pack()
+
+        self.third_info_label = tk.Label(self, text="Конечная единица измерения", font="Helvetica 15")
+        self.third_info_label.pack(pady=5)
 
         self.second_unit_selector = ttk.Combobox(self, values=list(self.units.keys()), justify='center',
                                                  textvariable=self.selected_unit_two)
         self.second_unit_selector['state'] = 'readonly'
-        self.second_unit_selector.grid(row=1, column=2)
+        self.second_unit_selector.pack()
 
-        self.first_entry = tk.Entry(self, width=24)
-        self.first_entry.grid(row=2, column=0, pady=70)
+        self.fourth_info_label = tk.Label(self, text="Результат конвертации", font="Helvetica 15")
+        self.fourth_info_label.pack(pady=5)
 
         self.second_entry = tk.Entry(self, width=24)
-        self.second_entry.grid(row=2, column=2, pady=70)
+        self.second_entry.pack()
         self.second_entry.configure(state='readonly')
 
         self.convert_button = tk.Button(self, text="Конвертировать", width=20, height=3, command=self.convert)
-        self.convert_button.grid(row=3, column=1)
-
+        self.convert_button.pack(pady=40)
         self.back_button = tk.Button(self, text="Назад", width=20, height=3,
                                      command=lambda: self.controller.show_frame(MAIN_FRAME_NAME))
-        self.back_button.grid(row=4, column=1)
+        self.back_button.pack()
 
     def render(self):
         self.__clear()
